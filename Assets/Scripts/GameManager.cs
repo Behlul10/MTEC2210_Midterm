2caaf8574c3fd4a8b2662cfe0fd69afb4f9cc4a1
Behlul10 <behlulvardal@gmail.com>
2024-03-04 21:24:39 -0500

    update on scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,26 +4,52 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [Range(1, 10)] public int itemCount;
-    public GameObject itemPrefab;
+    public GameObject coinPrefab;
+    public Transform northPoint;
+    public Transform southPoint;
+    public Transform westPoint;
+    public Transform eastPoint;
+
+    public float CoinSpawnDelay = 2;
+    float timeElapsed = 0; // by default if you dont state public or private it will be private
+
+    [Range(1, 10)] public int coinCount;
+    int currentCoinCount = 0;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        //for (int i = 0; i < itemCount; i++)
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
         
     }
 
-    /*
-    void SpawnItem(Vector3 location)
+    // Update is called once per frame
+    private void Update() 
     {
-        GameObject item = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
-        item.GetComponent<Object>().ObjectColor = Random.ColorHSV();
+       if (timeElapsed < CoinSpawnDelay) 
+        {
+            timeElapsed += Time.deltaTime;
+        }
+        else 
+        {
+            SpawnCoin();
+            timeElapsed = 0;
+        }
     }
-    */
+
+    public void SpawnCoin()
+    {
+        if (currentCoinCount < coinCount) 
+        {
+            GameObject coinClone = Instantiate(coinPrefab, SpawnPos(), Quaternion.identity);
+            currentCoinCount++;
+        }
+    }
+
+    private Vector2 SpawnPos() 
+    {
+        float xValue = Random.Range(westPoint.position.x, eastPoint.position.x);
+        float yValue = Random.Range(northPoint.position.y, southPoint.position.y);
+        Vector2 temp = new Vector2(xValue, yValue);
+        return temp;
+    }
 }
